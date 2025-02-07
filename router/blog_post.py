eab43d9658b69ff209462f57495144c61a98b35a
kristinekaposte <kika217@inbox.lv>
2025-02-07 14:54:13 +0200

    adding multiple values and complex subtypes

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,15 +1,22 @@
-from typing import Optional
+from typing import Optional, List, Dict
 
-from fastapi import APIRouter, Query, Body
+from fastapi import APIRouter, Query, Body, Path
 from pydantic import  BaseModel
 
 router = APIRouter(prefix="/blog", tags=["blog"])
+
+class Image (BaseModel):
+    url: str
+    alias: str
 
 class BlogModel(BaseModel):
     title: str
     content: str
     nb_comments: int
     published: Optional[bool]
+    tags: List[str]
+    metadata: Dict[str, str]
+    image: Optional[Image] = None
 
 # @router.post("/new")
 # def create_blog(blog: BlogModel):
@@ -23,19 +30,26 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         "version": version
     }
 
-@router.post('/new/{id}/comment')
+
+@router.post('/new/{id}/comment/{comment_id}')
 def create_comment(blog: BlogModel, id: int,
-                   comment_id: int = Query(None,
-                                           title='Id of the comment',
-                                           description='Some description for comment_id',
-                                           alias = "commentId",
+                   comment_title: int = Query(None,
+                                           title='Title of the comment',
+                                           description='Some description for comment_title',
+                                           alias = "commentTitle",
                                            deprecated=True
                                            ),
-                   content: str = Body("Hi how are you")
-                   ):
+                   # content: str = Body("Hi how are you") # default value
+                   content: str = Body(..., min_length=10, max_length=50, regex='^[a-z\s]*$'),
+                   # v: Optional[List[str]] = Query(None)
+                   v: Optional[List[str]] = Query(["1.0", "1.1", "1.2"]),  # default values
+                   comment_id: int = Path(..., gt=5, le=10)
+    ):
     return {
         "id": id,
-        "comment_id": comment_id,
+        "comment_title": comment_title,
         "blog": blog,
-        "content": content
+        "content": content,
+        "version": v,
+        "comment_id": comment_id,
     }
