5c32f9a50694e2e0cbe1099df8a05244c9c538e2
kristinekaposte <kika217@inbox.lv>
2025-02-18 14:40:12 +0200

    Adding user Relationships with article

--- db/models.py
+++ db/models.py
@@ -1,7 +1,10 @@
 # Model definition
+from sqlalchemy.orm import relationship
+
 from db.database import Base
 from sqlalchemy import Column
 from sqlalchemy.sql.sqltypes import Integer, String, Boolean
+from sqlalchemy.sql.schema import ForeignKey
 
 
 class DbUser(Base):
@@ -10,3 +13,13 @@ class DbUser(Base):
     username = Column(String, unique=True)
     email = Column(String, unique=True)
     password = Column(String)
+    items = relationship("DbArticle", back_populates="user") # this will create a relationship with the articles table
+
+class DbArticle(Base):
+    __tablename__ = "articles"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    published = Column(Boolean)
+    user_id = Column(Integer, ForeignKey("users.id")) # this will create a foreign key relationship with the users table
+    user = relationship("DbUser", back_populates="items") # this will create a relationship with the users table
--- schemas.py
+++ schemas.py
@@ -1,15 +1,60 @@
 # scema definition for the database
 # will get info from api and then store it in the database
+from typing import List
+
 from pydantic import BaseModel
+
+# Article inside UserDisplay
+class Article(BaseModel):
+    title: str
+    content: str
+    published: bool
+    class Config:
+        from_attributes = True
 
 class UserBase(BaseModel):
     username: str
     email: str
     password: str
 
+# class UserDisplay(BaseModel):
+#     username: str
+#     email: str
+#     items: List[Article] = []
+#     class Config:
+#         from_attributes = True  # this will allow us to get the data from the database and display it in the api
+
 class UserDisplay(BaseModel):
     username: str
     email: str
+    items: List[Article]
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if 'items' not in data:
+            self.items = []
 
     class Config:
-        from_attributes = True  # this will allow us to get the data from the database and display it in the api
+        from_attributes = True
+
+
+#User inside ArticleDisplay
+class User(BaseModel):
+    id: int
+    username: str
+    class Config:
+        from_attributes = True
+
+class ArticleBase(BaseModel):
+    title: str
+    content: str
+    published: bool
+    creator_id: int  # user_id
+
+class ArticleDisplay(BaseModel):
+    title: str
+    content: str
+    published: bool
+    user: User
+    class Config:
+        from_attributes = True
