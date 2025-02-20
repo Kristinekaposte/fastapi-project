21b5992bb44521d0e90ec388c4e39488b2b363a9
kristinekaposte <kika217@inbox.lv>
2025-02-20 14:59:33 +0200

    Adding cookies, form data

--- router/product.py
+++ router/product.py
@@ -1,7 +1,8 @@
+from http.client import responses
 from typing import Optional, List
 
 from fastapi.responses import Response, HTMLResponse, PlainTextResponse
-from fastapi import Header
+from fastapi import Header, Cookie, Form
 from fastapi import APIRouter
 
 router = APIRouter(
@@ -12,24 +13,38 @@ router = APIRouter(
 products = ["watch", "phone", "laptop"]
 
 
+"""
+The Form(...) in the function parameter is used to indicate 
+that the name parameter should be taken from form data in a POST request. 
+The ellipsis (...) is used to indicate that this parameter is required.
+"""
+@router.post("/new")
+def create_product(name: str = Form(...)):
+    products.append(name)
+    return products
+
+
 @router.get("/all")
 def get_all_products():
     data = " ".join(products)
-    return Response(content=data, media_type="text/plain")
+    response = Response(content=data, media_type="text/plain")
+    response.set_cookie(key="test_cookie", value="test_cookie_value")
+    return response
 
 
 @router.get("/withheader")
 def get_products(
         response: Response,
-        custom_header: Optional[List[str]] = Header(None)
-        ):
-    response.headers["custom_response_header"] = ", ".join(custom_header)
-    return products
-
-
-
-
-
+        custom_header: Optional[List[str]] = Header(None),
+        test_cookie: Optional[str] = Cookie(None)
+):
+    if custom_header:
+        response.headers["custom_response_header"] = ", ".join(custom_header)
+    return {
+        "data": products,
+        "custom_header": custom_header,
+        "my_cookie": test_cookie
+    }
 
 
 @router.get("/{id}", responses={
