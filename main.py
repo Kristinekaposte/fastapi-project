0bd1b74174223b01129c16ea2c629c6f9e799954
kristinekaposte <kika217@inbox.lv>
2025-02-24 17:54:09 +0200

    adding templates, async, websocket endpoint

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
+from os import name
+from fastapi import responses
+from fastapi import websockets
 from fastapi.exceptions import HTTPException
-
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.responses import PlainTextResponse
+from starlette.responses import HTMLResponse
 from exceptions import StoryException
-from router import blog_get, blog_post, article, product, user, file
+from fastapi import FastAPI
+from router import blog_get, blog_post, user, article, product, file
+from auth import authentication
+from templates import templates
 from db import models
 from db.database import engine
-from auth import authentication
+from fastapi import Request
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+import time
+from client import html
+from fastapi.websockets import WebSocket
 
-# run server with: uvicorn main:app --reload
 app = FastAPI()
+app.include_router(templates.router)
 app.include_router(authentication.router)
 app.include_router(file.router)
-app.include_router(blog_get.router)
-app.include_router(blog_post.router)
 app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(product.router)
+app.include_router(blog_get.router)
+app.include_router(blog_post.router)
 
 
-@app.get("/")
-def index():
-    return {"message": "Hello World"}
+# @app.get("/hello")
+# def index():
+#     return {"message": "Hello World"}
 
 
 @app.exception_handler(StoryException)
@@ -32,6 +41,21 @@ def story_exception_handler(request: Request, exc: StoryException):
         status_code=418,
         content={"detail": exc.name}
     )
+
+@app.get("/")
+async def get():
+  return HTMLResponse(html)
+
+clients = []
+
+@app.websocket("/chat")
+async def websocket_endpoint(websocket: WebSocket):
+  await websocket.accept()
+  clients.append(websocket)
+  while True:
+    data = await websocket.receive_text()
+    for client in clients:
+      await client.send_text(data)
 
 
 # @app.exception_handler(HTTPException)
@@ -55,3 +79,7 @@ app.add_middleware(
 )
 
 app.mount('/files', StaticFiles(directory="files"), name='files')
+app.mount('/templates/static',
+      StaticFiles(directory="templates/static"),
+      name="static"
+)
